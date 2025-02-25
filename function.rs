3224617c65c237cd33f8f4381e670096f6c214f2
MoHamed1hp <m7md1hp@gmail.com>
2025-02-26 00:31:18 +0200

    functions

--- function.rs
+++ function.rs
@@ -7,7 +7,15 @@ fn main() {
         let qnt = 10;
         price * qnt
     };
-    println!("total is : {}",total_lol)
+    println!("total is : {}",total_lol);
+
+    let lol:i32 = add(5,5);
+    println!("the sum is : {}",lol);
+
+    let weight:f64 = 70.0;
+    let height:f64 = 1.82;
+    let bmi = calculate_bmi(weight , height);
+    println!("BMI is : {:.2}",bmi);
 }
 
 
@@ -23,3 +31,12 @@ fn human_id(name:&str,age:u8,country:&str){
     println!("The name is {} the age is {} and country {}",name,age ,country)
 }
 
+fn add(x:i32 ,y:i32) -> i32{    //return type 
+    x+y
+}
+
+//BMI = weight / height^2
+
+fn calculate_bmi(weight: f64, height: f64) -> f64{
+    weight / height.powi(2)
+}
